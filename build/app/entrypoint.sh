b7e0ac0291d21bcaacd3473f186f450b45b93268
RahmaanM17 <ardrah17@gmail.com>
2026-04-13 23:12:54 +0700

    chore: key generate

--- build/app/entrypoint.sh
+++ build/app/entrypoint.sh
@@ -6,6 +6,8 @@ cd /var/www/html
 # The compose bind mount can reintroduce stale local cache manifests that reference
 # dev-only providers such as Laravel Pail. Rebuild them against the installed vendor set.
 rm -f bootstrap/cache/*.php
+php artisan key:generate --force
 php artisan package:discover --ansi
+php artisan schedule:work --no-interaction --verbose &
 
 exec "$@"

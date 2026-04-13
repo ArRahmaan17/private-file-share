2f8d90e27c2c8b18d4ad31b5326bd02c62742e9f
RahmaanM17 <ardrah17@gmail.com>
2026-04-14 00:59:41 +0700

    style: open graph on head

--- applications/resources/views/components/head.blade.php
+++ applications/resources/views/components/head.blade.php
@@ -5,6 +5,7 @@
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
+<meta property="og:logo" content="{{ asset('favicon.ico') }}">
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:title" content="{{ $title }}">
 <meta property="og:description" content="{{ $description }}">

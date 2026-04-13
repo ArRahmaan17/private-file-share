889b67523e5716188a70347f6f118317305bb4bf
RahmaanM17 <ardrah17@gmail.com>
2026-04-14 00:40:15 +0700

    chore: docker build process

--- build/app/entrypoint.sh
+++ build/app/entrypoint.sh
@@ -5,7 +5,10 @@ cd /var/www/html
 
 # The compose bind mount can reintroduce stale local cache manifests that reference
 # dev-only providers such as Laravel Pail. Rebuild them against the installed vendor set.
-rm -f bootstrap/cache/*.php
+rm -f bootstrap/cache/*.php &
+php artisan config:clear &
+php artisan cache:clear &
+php artisan route:clear &
 php artisan schedule:work --no-interaction --verbose &
 
 exec "$@"

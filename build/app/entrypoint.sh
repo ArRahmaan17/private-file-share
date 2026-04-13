e7061f07928bc5119eb8d0e37ed20b5d926246dc
RahmaanM17 <ardrah17@gmail.com>
2026-04-13 23:18:36 +0700

    chore: migrate

--- build/app/entrypoint.sh
+++ build/app/entrypoint.sh
@@ -6,9 +6,7 @@ cd /var/www/html
 # The compose bind mount can reintroduce stale local cache manifests that reference
 # dev-only providers such as Laravel Pail. Rebuild them against the installed vendor set.
 rm -f bootstrap/cache/*.php
-php artisan key:generate --force
 php artisan package:discover --ansi
-php artisan migrate
 php artisan schedule:work --no-interaction --verbose &
 
 exec "$@"
